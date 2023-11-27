b391ce7bb60ddaf91810a70fb568c062af059309
Cole Hansen <96995050+colehansen28@users.noreply.github.com>
2023-11-26 19:56:20 -0500

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,14 @@
+
+
+mobiscroll.datepicker('#inline-picker',{
+    contols: ['calendar'],
+    display: 'inline',
+    touchUi: true
+})
+
+
+
+mobiscroll.setOptions({
+    theme: 'windows',
+    themeVariant: 'light',
+});
